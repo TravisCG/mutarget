6289e468b05554427df3b802d666250dc2ac04f8
TravisCG <buldozer88@hotmail.com>
2018-06-05 13:35:02 +0200

    Picture creation implemented

--- createpic.R
+++ createpic.R
@@ -1,10 +1,14 @@
-drawboxplot <- function(m, tmpp, n, mut, gname){
+drawboxplot <- function(m, ingene, tmpp, n, mut, gname, outliers){
 	png(paste(tmpp, gname, "png", sep = "."))
 	if(m == "genotype"){
-		boxplot()
+		muts <- mut[mut$genes == ingene,] == 1
+		wts  <- mut[mut$genes == ingene,] == 0
+		boxplot(as.numeric(n[n$genes == gname, muts]), as.numeric(n[n$genes == gname, wts]), main = paste(gname, "expression", sep=" "), names = c("Mutant", "WT"), outline = outliers)
 	}
 	else{
-		boxplot()
+		muts <- mut[mut$genes == gname,] == 1
+		wts  <- mut[mut$genes == gname,] == 0
+		boxplot(as.numeric(n[n$genes == ingene, muts]), as.numeric(n[n$genes == ingene, wts]), main = paste(ingene, "expression", sep = " "), names = c("Mutant", "WT"), xlab = paste(gname, "mutation status", sep = " "), outline = outliers)
 	}
 	dev.off()
 }
@@ -17,6 +21,7 @@ resultfile <- argv[4]
 expfile    <- argv[5]
 mutfile    <- argv[6]
 plotnum    <- as.numeric(argv[7])
+outliers   <- as.numeric(argv[8]) == 1
 
 normexp <- read.table(expfile, header = T, check.names = F)
 mut     <- read.table(mutfile, header = T, check.names = F)
@@ -24,5 +29,5 @@ result  <- read.table(resultfile, header = T, check.names = F)
 
 for(index in 1:plotnum){
 	genename <- result[index,1]
-	drawboxplot(mode, tmpprefix, normexp, mut, genename)
+	drawboxplot(mode, inputgene, tmpprefix, normexp, mut, genename, outliers)
 }
